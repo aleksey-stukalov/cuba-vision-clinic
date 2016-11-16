cab6d7433d2d360f7aadf670eb0c6763329e7498
Olga Shiryaeva <shiryaeva@haulmont.com>
2016-11-16 17:08:34 +0400

    comments, copyright

--- modules/web/src/com/company/cubavisionclinic/web/product/ProductBrowse.java
+++ modules/web/src/com/company/cubavisionclinic/web/product/ProductBrowse.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Haulmont
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.company.cubavisionclinic.web.product;
 
 import com.company.cubavisionclinic.entity.Product;
@@ -120,7 +136,7 @@ public class ProductBrowse extends AbstractLookup {
         });
 
         /**
-         * Handles clear button clicked event, asking for action confirmation
+         * Handles the clear button clicked event, asking for action confirmation
          * and clearing the {@link Product#productImage} field if confirmed
          */
         imageUpload.addBeforeValueClearListener(beforeEvent -> {
